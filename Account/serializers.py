eda7f753a6b6cbd4d9ca55b3d24b03d8a5d631fd
kyounghoon <kimkh19@koreaap.com>
2021-08-19 08:13:25 +0900

    로그인 직접 구현 최종 2가지 버전

--- Account/serializers.py
+++ Account/serializers.py
@@ -7,44 +7,40 @@ JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
 JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
 
+'''
+    CASE 1 :
+    User 직렬화를 위한 선언
+'''
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
-
+'''
+    CASE 2 :
+    유효성 검증만 처리 후, 인자값에 대한 결과값 반환.
+'''
 class LoginUserAccountSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
-    def validate(self, attrs):
 
-        message = ''
-        success = True
+    def validate(self, attrs):
 
         username = attrs.get('username')
         password = attrs.get('password')
-        
-        user = authenticate(username=username, password=password)
 
-        if user is None:
-            message = 'ID와 비밀번호를 확인해주세요.'
-            success = False
-
-        try:
-            payload = JWT_PAYLOAD_HANDLER(user)
-            jwt_token = JWT_ENCODE_HANDLER(payload)
-        except User.DoesNotExist:
+        if username == "":
             raise serializers.ValidationError(
-                '로그인 오류입니다. 잠시 후에 시도해주세요'
+                '사용자 ID를 입력해 주세요.'
             )
 
+        if len(password) < 8:
+            raise serializers.ValidationError(
+                '비밀번호는 최소 8자리 입니다.'
+            )
 
-        return {
-            'user': user,
-            'token': jwt_token,
-            'message': message,
-            'success': success
-        }
-    
+        return attrs
+
+
--- Account/views.py
+++ Account/views.py
@@ -1,15 +1,18 @@
 import json
-from rest_framework import serializers
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes 
 from rest_framework.permissions import AllowAny, IsAuthenticated 
 from django.http import HttpResponse
 
-from .serializers import LoginUserAccountSerializer
+from .serializers import UserSerializer, LoginUserAccountSerializer
 from rest_framework.views import APIView
-from rest_framework import generics
 
-from django.contrib.auth.models import User
-from django.core import serializers
+from rest_framework_jwt.settings import api_settings
+from rest_framework.response import Response
+
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
 
 
 
@@ -19,59 +22,92 @@ def index(request):
     return HttpResponse('hello')
 
 
-class LoginUserAccount(generics.GenericAPIView):
-    serializer_class = LoginUserAccountSerializer
+
+'''
+    CASE 1 :
+    UserSerializer로 직렬화
+'''
+class LoginUserAccount(APIView):
+
     permission_classes = [AllowAny]
+    
+    def post(self, request):  
 
-    def post(self, request, *args, **kwrags):
+        result = {
+            'user': None,
+            'token': '',
+            'success': True,
+            'message': ''
+        }
+        token = ''
 
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception = True)
+        username = request.data['username']
+        password = request.data['password']
 
-        data = serializer.validated_data
+        user = authenticate(username=username, password=password)
 
-        token = data.get('token')
-        user = data.get('user')
-        success = data.get('success')
-        message = data.get('message')
+        if user is None:
+            result['success'] = False
+            result['message'] = '존재하지 않은 회원입니다.'
+        else:
+            try:
+                payload = JWT_PAYLOAD_HANDLER(user)
+                token = JWT_ENCODE_HANDLER(payload)
+            except:
+                result['message'] = '토큰의 정보를 가져오지 못했습니다.'
+                result['success'] = False
+
+
+        serializer = UserSerializer(user, data=request.data)
+        serializer.is_valid()
+
+        result['user'] = serializer.data
+        result['token'] = token
+
+        return Response(result, content_type='application/json')
+
+
+'''
+    CASE 2:
+    유효성 검증 처리 후, 직렬화를 통해 데이터 반환
+    유효성 검증 에러 시, 400 에러 반환
+'''
+class LoginUserAccount(APIView):
+
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serializer = LoginUserAccountSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        message = ''
+        token = ''
+
+        username = serializer.data['username']
+        password = serializer.data['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            message = 'ID와 비밀번호를 확인해주세요.'
+            success = False
+        else:
+            userSerializer = UserSerializer(user, data=serializer.data)
+            userSerializer.is_valid()
+            success = True
+
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            token = JWT_ENCODE_HANDLER(payload)
+        except:
+            message = '토큰의 정보를 가져오지 못했습니다.'
 
         response = {
+            'user': userSerializer.data,
             'token': token,
             'success': success,
-            'message': message,
-            'user': {
-                'pk': user.pk,
-                'username': user.username,
-                'email': user.email
-            }
+            'message': message
         }
 
         return HttpResponse(json.dumps(response), content_type='application/json')
-
-
-
-# class LoginUserAccount(APIView):
-    
-#     permission_classes = [AllowAny]
-#     serializer_class = LoginUserAccountSerializer
-
-#     def post(self, request, *args, **kwargs):
-        
-#         serializer = LoginUserAccountSerializer(data=request.data)
-
-#         if not serializer.is_valid(raise_exception=True):
-#             return HttpResponse({"message": "Request Body Error."})
-
-#         response = {
-#             'success': True,
-#             'context': serializer.data
-#         }
-
-#         print(response['context'])
-
-#         return HttpResponse(response['context'])
-
-
-
-
 
