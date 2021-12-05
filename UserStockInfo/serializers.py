5a2b3efdd0a9820db31eb90f05253409dea810da
kyounghoon <kkh2826@naver.com>
2021-12-05 20:46:39 +0900

    Add get/post/delete in views.py

--- UserStockInfo/serializers.py
+++ UserStockInfo/serializers.py
@@ -2,20 +2,18 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import UserStockInfo
 
+
 class UserStockInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserStockInfo
         fields = ['id', 'stockCode', 'buysellFlag', 'buyDate', 'buyAmount', 'buyPrice']
 
-    # def create(self, request, *args, **kwargs):
-    #     is_many = isinstance(request.data, list)
-        
+    def create(self, validated_data):
 
-    # def update(self, instance, validated_data):
-    #     instance.stockCode = validated_data.get('stockCode', instance.stockCode)
-    #     instance.buysellFlag = validated_data.get('buysellFlag', instance.buysellFlag)
-    #     instance.buyDate = validated_data.get('buyDate', instance.buyDate)
-    #     instance.buyAmount = validated_data.get('buyAmount', instance.buyAmount)
-    #     instance.buyPrice = validated_data.get('buyPrice', instance.buyPrice)
-    #     return instance
+        if UserStockInfo.objects.filter(user=validated_data['user'], stockCode=validated_data['stockCode']).count() > 0:
+            answer = UserStockInfo.objects.filter(user=validated_data['user'], stockCode=validated_data['stockCode']).update(**validated_data)
+        else:
+            answer = UserStockInfo.objects.create(**validated_data)
+
+        return answer
--- UserStockInfo/views.py
+++ UserStockInfo/views.py
@@ -25,42 +25,63 @@ def InitResult() :
 
 # Create your views here.
 class UserStockInfoByUser(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JSONWebTokenAuthentication]
+
     '''
         사용자의 주식정보를 가져온다.
     '''
-
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JSONWebTokenAuthentication]
 
     def get(self, request):
         userStockInfo = UserStockInfo.objects.filter(user__username=request.user.username)
         serializer = UserStockInfoSerializer(userStockInfo, many=True)
         return Response(serializer.data, content_type='application/json')
 
+    '''
+        사용자의 주식정보를 저장한다.
+    '''
+
     def post(self, request):
-        
+
         result = InitResult()
 
         userStockInfo = json.loads(request.body)
-        user = request.user
+        user = User.objects.get(username=request.user.username)
 
-        #for userStock in userStockInfo['UserStockInfo']:
-        userStockInfoSerializer = UserStockInfoSerializer(instance=user, data=userStockInfo["UserStockInfo"], many=True)
-
-        if userStockInfoSerializer.is_valid():
-            for userStock in userStockInfoSerializer.data:
-                userStock.user
-                print(userStock)
-
-        # if usi.is_valid():
-        #     print('hi')
-        #     usi.save()
-        # except:
-        #     result['success'] = False
-        #     result['message'] = "저장실패"
-        #     return Response(result, content_type='application/json')
+        for userStock in userStockInfo["UserStockInfo"]:
+            try:
+                userStockSerializer = UserStockInfoSerializer(data=userStock)
+                userStockSerializer.is_valid()
+                userStockSerializer.save(user=user)
+            except:
+                result['success'] = False
+                result['message'] = "저장실패"
+                return Response(result, content_type='application/json')
 
         result['success'] = True
         result['message'] = "저장성공"
         
+        return Response(result, content_type='application/json')
+
+
+    def delete(self, request):
+
+        result = InitResult()
+
+        userStockInfo = json.loads(request.body)
+
+        userStockInfo = userStockInfo["UserStockInfo"]
+        user = User.objects.get(username=request.user.username)
+
+        try:
+            UserStockInfo.objects.filter(user=user, stockCode=userStockInfo["stockCode"]).delete()
+        except:
+            result['success'] = False
+            result['messsage'] = "삭제실패"
+            return Response(result, content_type='application/json')
+
+        result['success'] = True
+        result['message'] = '삭제성공'
+
         return Response(result, content_type='application/json')
